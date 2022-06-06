ee9504a8df2383cfb67598d6d4eec742be5b1262
dkunzman77 <douginsaintlouis@gmail.com>
2022-06-05 21:15:40 -0500

    buttons exists with component

--- src/pages/AllTodos/index.tsx
+++ src/pages/AllTodos/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import { Todo } from '../../components/Todo'
+import React, { useState } from 'react'
 import { useTodos } from '../../hooks'
-
+import { AllToDosComponent } from './allToDos'
 /**
  * ### Page
  *
@@ -9,25 +8,47 @@ import { useTodos } from '../../hooks'
  * status.
  */
 const AllTodos: React.FC = () => {
-  const { allTodos, isLoading, isError } = useTodos()
+  const { allTodos, todos, isLoading, isError } = useTodos()
   console.warn(allTodos)
+  console.warn(todos)
+  const [show, setShow] = useState('all')
 
+  const handleClick = (value: string) => {
+    setShow(value)
+  }
+  let component: JSX.Element[] | JSX.Element = <></>
+
+  if (isLoading) {
+    component = <h2>Loading...</h2>
+  } else if (isError) {
+    component = <h2>Error...</h2>
+  } else {
+    switch (show) {
+      case 'all':
+        component = (
+          <>
+            <AllToDosComponent todos={todos} />
+          </>
+        )
+        break
+      // case 'complete':
+      //   component = <CompleteToDosComponent todos={todos} />
+      //   break
+      // case 'incomplete':
+      //   component = <InCompleteToDosComponent todos={todos} />
+      //   break
+    }
+  }
   return (
     <>
       <h1 style={{ fontWeight: 800, fontSize: 24 }}>All Todos</h1>
       <br /> <br />
-
-      {allTodos &&
-        !isError &&
-        !isLoading &&
-        allTodos.map((todo, idx) => (
-          <Todo
-            key={idx}
-            id={todo.id}
-            title={todo.title}
-            completed={todo.completed}
-          />
-        ))}
+      <div>
+        <button onClick={() => handleClick('all')}>All </button>
+        <button onClick={() => handleClick('complete')}>Complete</button>
+        <button onClick={() => handleClick('incomplete')}>Incomplete</button>
+      </div>
+      {component}
     </>
   )
 }
